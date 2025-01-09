4d9553c55ddbbdff9ef53a1736889f13bb6c7a80
Daniel Ocando <daniel.ocando955@gmail.com>
2025-01-09 18:16:14 -0300

    converted index to html for easier serving

---
+++ site.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>LatinTub</title>
+    <?php include_once 'util/head.php' ?>
+    <link rel="stylesheet" href="css/landing.css">
+  </head>
+  <body>
+    <?php include 'util/header.php' ?>
+    <div class="w-100 h-100 container-fluid" id="content">
+      <div class="row align-items-center background my-5 w-100 mx-auto" id="landing" >
+        <h1 class="text-center">LATINTUB</h1>
+        <h2 class="text-center"> Sabemos que para cada problema planteado existe una respuesta &oacute;ptima.  </h2>
+      </div>
+      <div class="container">
+        <div class="row text-justify">
+          <p class="col-12 text-justify">
+			Latintub es una empresa de servicios de ingenieria y consultoria con mas de 20 años de experiencia en la industria petrolera. 
+          </p>
+        </div>
+        <div class="anchor justify-content-center section-sep my-4 row" id="about">
+          <div class="col-6 col-lg-3 mb-3">
+            <img class="w-100" src="img/DANI.png" alt="">
+          </div>
+          <div class="col-6 col-lg-5 mb-3">
+            <p class="comp-name">Daniel Sauthier</p>
+            <p>Ingeniero Mecánico <br>Master en Economía y Administración de Empresas ESEADE.</p>
+            <a class="" href="#DaniMoreInfo" data-bs-toggle="collapse" data-bs-target=".DaniMoreInfo" aria-expanded="false" aria-controls="DaniMoreInfo">Experiencia</a>
+          </div>
+          <div class="wl-100"></div>
+          <ul class="col-12 col-lg-6 collapse DaniMoreInfo">
+            <li>
+              Transporte por tuberías, diseño, construcción, operación y
+              mantenimiento de gasoductos y oleoductos.     
+              Desarrollo de nuevos negocios de gasoductos regionales, en Uruguay, Chile
+              y Brasil.     
+            </li>
+            <li>
+              Implementación de programas de integridad de
+              oleoductos y gasoductos.    
+            </li>
+            <li>
+              Representante de compañías
+              internacionales en inspección interna, integridad y
+              mantenimiento de tuberías, tanto onshore como offshore. 
+            </li>
+          </ul>
+          <ul class="col-12 col-lg-6 collapse DaniMoreInfo">
+            <li>
+              Transporte por tuberías, diseño, construcción, operación y
+              mantenimiento de gasoductos y oleoductos.     
+              Desarrollo de nuevos negocios de gasoductos regionales, en Uruguay, Chile
+              y Brasil.     
+            </li>
+            <li>
+              Implementación de programas de integridad de
+              oleoductos y gasoductos.    
+            </li>
+            <li>
+              Representante de compañías
+              internacionales en inspección interna, integridad y
+              mantenimiento de tuberías, tanto onshore como offshore. 
+            </li>
+          </ul>
+        </div>
+        <div class="anchor row" id="energy">
+        <div class="anchor row my-4 section-sep" id="experts">
+          <h2 class="col-4"><span class="comp-name">LATINTUB</span> ENERGIA</h2> 
+          <h3 class="col-8">Consultoría en organización, optimización, mejora de procesos internos de Empresas.</h3>
+          <div class="d-flex align-items-start my-3">
+          <div class="nav flex-column nav-pills me-3 col-4" id="v-experts-tab" role="tablist" aria-orientation="vertical">
+            <button class="nav-link active" 
+                    id="v-pills-linePipe"
+                    data-bs-toggle="pill" 
+                    data-bs-target="#linePipe"
+                    type=button 
+                    role="tab" 
+                    aria-controls="v-energy-tab"
+                    aria-selected="true">
+              Tuberia de Conduccion
+            </button>
+            <button class="nav-link" 
+                    id="v-pills-tubing"
+                    data-bs-toggle="pill" 
+                    data-bs-target="#tubing"
+                    type=button 
+                    role="tab" 
+                    aria-controls="v-energy-tab"
+                    aria-selected="false">
+              Tuberia de pozos
+            </button>
+            <button class="nav-link" 
+                    id="v-pills-oilAndGas"
+                    data-bs-toggle="pill" 
+                    data-bs-target="#oilAndGas"
+                    type=button 
+                    role="tab" 
+                    aria-controls="v-energy-tab"
+                    aria-selected="false">
+              Operaciones de petroleo y gas
+            </button>
+          </div>
+          <div class="tab-content col-8" id="v-expertsContent">
+            <ul id="linePipe" 
+              class="tab-pane fade show active" 
+              role="tabpanel" 
+              aria-labelledby="v-pills-linePipe" 
+              tabindex="0">
+              <li>Ingieneria de ductos</li>
+              <li>Auditoria y certificacion de proyectos, obras y producto</li>
+              <li>Rehabilitacion de ductos</li>
+              <li>Operacion y mantenimiento de tuberias</li>
+              <li>Revision de ingieneria de proyectos</li>
+              <li>Estudios de integridad</li>
+            </ul>
+            <ul id="tubing" 
+              class="tab-pane fade" 
+              role="tabpanel" 
+              aria-labelledby="v-pills-tubing" 
+              tabindex="0">
+              <li>Seleccion, especificacion de productos</li>
+              <li>Conexiones Premium - Soporte en fabricacion/roscado, mejora de procesos.</li>
+			  <li>Procedimientos de control</li>
+			  <li>Diseno de columnas tubulares de pozos</li>
+			  <li>Soporte, verificacion, chequeo</li>
+			  <li>Desarrollo de documentacion tecnica</li>
+			  <li>Analisis de falla y optimizacion de productos</li>
+			  <li>Inspeccion visual de productos tubulares</li>
+			  <li>Inspeccion y auditoria de fabricacion de producto y ensayos de certificacion</li>
+            </ul>
+            <ul id="oilAndGas" 
+              class="tab-pane fade"
+              role="tabpanel" 
+              aria-labelledby="v-pills-oilAndGas" 
+              tabindex="0">
+              <li>Servicios a campos petroleros</li>
+              <li>Diseno y seguimiento</li>
+              <li>Servicios de asistencia a operaciones de campo (instalacion de tuberias)</li>
+              <li>Supervision de armado de accesorios</li>
+			  <li>Roscados y apretados de conexiones</li>
+			  <li>Mejora de operaciones de produccion</li>
+            </ul>
+          </div>
+          </div>
+          </div>
+        </div>
+        <div class="anchor row section-sep my-3 py-3" id="work_done">
+          <h1 class="col-12">Trabajos Realizados</h1> 
+          <ul class="col-8">
+            <li>Estudios de adecuaci&oacute;n del muelle del ex-Arsenal Naval
+              Rio Santiago y evaluaci&oacute;n preliminar del dragado del
+              canal intermedio, Ensenada, Pcia de Buenos Aires. (a&ntilde;o
+              2009 a 2013)</li> 
+            <li>Ingenier&iacute;a de rehabilitaci&oacute;n
+              tuber&iacute;a submarina terminal mar&iacute;timo de ARICA
+              (Chile) para YPFB Transportes. (a&ntilde;o 2010)</li>
+            <li>Dise&ntilde;o conceptual e ingenier&iacute;a b&aacute;sica toma
+              de agua para refrigeraci&oacute;n de futura central t&eacute;rmica
+              de Ensenada de Barrag&aacute;n, Pcia de Buenos Aires. (a&ntilde;o
+              2011)</li>
+            <li>Estudios de adecuaci&oacute;n del muelle del ex-Arsenal Naval
+              Rio Santiago y evaluaci&oacute;n preliminar del dragado del
+              canal intermedio, Ensenada, Pcia de Buenos Aires. (a&ntilde;o
+              2009 a 2013)</li> 
+            <li>Ingenier&iacute;a de rehabilitaci&oacute;n
+              tuber&iacute;a submarina terminal mar&iacute;timo de ARICA
+              (Chile) para YPFB Transportes. (a&ntilde;o 2010)</li>
+            <li>Dise&ntilde;o conceptual e ingenier&iacute;a b&aacute;sica toma
+              de agua para refrigeraci&oacute;n de futura central t&eacute;rmica
+              de Ensenada de Barrag&aacute;n, Pcia de Buenos Aires. (a&ntilde;o
+              2011)</li>
+          </ul>
+          <img class="col-4" src="img/Stock_pipe02.jpg" alt="">
+          <img class="col-4" src="img/Stock_pipe03.jpg" alt="">
+          <div class="col-8">
+            <ul>
+              <li>Estudios de adecuaci&oacute;n del muelle del ex-Arsenal Naval
+                Rio Santiago y evaluaci&oacute;n preliminar del dragado del
+                canal intermedio, Ensenada, Pcia de Buenos Aires. (a&ntilde;o
+                2009 a 2013)</li> 
+              <li>Ingenier&iacute;a de rehabilitaci&oacute;n
+                tuber&iacute;a submarina terminal mar&iacute;timo de ARICA
+                (Chile) para YPFB Transportes. (a&ntilde;o 2010)</li>
+              <li>Dise&ntilde;o conceptual e ingenier&iacute;a b&aacute;sica toma
+                de agua para refrigeraci&oacute;n de futura central t&eacute;rmica
+                de Ensenada de Barrag&aacute;n, Pcia de Buenos Aires. (a&ntilde;o
+                2011)</li>
+              <li>Estudios de adecuaci&oacute;n del muelle del ex-Arsenal Naval
+                Rio Santiago y evaluaci&oacute;n preliminar del dragado del
+                canal intermedio, Ensenada, Pcia de Buenos Aires. (a&ntilde;o
+                2009 a 2013)</li> 
+              <li>Ingenier&iacute;a de rehabilitaci&oacute;n
+                tuber&iacute;a submarina terminal mar&iacute;timo de ARICA
+                (Chile) para YPFB Transportes. (a&ntilde;o 2010)</li>
+            </ul>
+          </div>
+        </div>
+        <div class="anchor row text-align-center justify-content-center" id="contact">
+          <h1 class="col-3">Contacto</h1> 
+          <div class="w-100"></div>
+          <div class="col-4">
+            <ul>
+              <li>Telefono: +54 9 11 6365 2355</li>
+              <li>Mail: latintub@gmail.com</li>
+              <li>Direccion: CABA, Conesa 1955 8vo 26 (1431)</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <?php include 'util/footer.php' ?>
+    </div>
+  </body>
+</html>
